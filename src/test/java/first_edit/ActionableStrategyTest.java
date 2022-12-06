caf06cf434a8def30c1ff2f7238d0283ab69da7d
lia stith <lia.stith@gmail.com>
2022-12-06 12:57:26 -0600

    enforcing Proper naming conventions for test class

--- src/test/java/first_edit/ActionableStrategyTest.java
+++ src/test/java/first_edit/ActionableStrategyTest.java
@@ -10,21 +10,21 @@ class ActionableStrategyTest {
 
 first_edit.Player Player = new Player();
    @Test
-   void LootPositiveTest(){
+   void lootPositiveTest(){
        Assertions.assertEquals("you hit the jackpot you got a gold coin",loot.action(Player,'l'));
    }
     @Test
-    void LootnegativeTest(){
+    void lootNegativeTest(){
         Assertions.assertEquals("Invalid input",loot.action(Player,'t'));
 
     }
 
     @Test
-    void InteractPositiveTest(){
+    void interactPositiveTest(){
         Assertions.assertEquals("you touched the walls and discover a game pixel ",interaact.action(Player,'i'));
     }
     @Test
-    void InteraactnegativeTest(){
+    void interactNegativeTest(){
         Assertions.assertEquals("Invalid input",interaact.action(Player,'t'));
     }
     @Test
@@ -32,7 +32,7 @@ first_edit.Player Player = new Player();
         Assertions.assertEquals("you made it to the end woooow i didnt like that ",exit.action(Player,'x'));
     }
     @Test
-    void exitnegativeTest(){
+    void exitNegativeTest(){
         Assertions.assertEquals("Invalid input",exit.action(Player,'t'));
     }
 
